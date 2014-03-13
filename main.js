b2710c4eea465e66e45c59bf1633f2ac703ba3ea
Paul Szerlip <paul.szerlip@knights.ucf.edu>
2014-03-12 21:50:00 -0400

    Excellent. Wired up the individual creation, visible, and hidden callbacks.
    Set up the loading objects, and fixed the height of the bottom click buttons. I've also updated some dependencies, which are used for class toggling, and click handling.
    
    Added like button behavior that toggles a "like" class to indicate visually that an object is selected. Way better than WPF, man i hate xaml.
    
    Next step: wiring up like/publish callbacks for model behavior and actual parent interactions. Flexforever can't be used for parent behavior. Also, create individual needs to send parental information over the wire (so a list of parents would be best).

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 var Swiper = require('swiper');
 var flexforever = require('flexforever');
 var Emitter = require('emitter');
+var events = require('events');
+var classes = require('classes');
 
 module.exports = flexIEC; 
 
@@ -67,90 +69,194 @@ var createParentContainer = function(id, classes)
 
 var createRow = function(id, additionalClasses)
 {
-	return createDiv(id, "row rowOR" + (additionalClasses ? (" " + additionalClasses) : "")) ;
+	return createDiv(id, "row mOR" + (additionalClasses ? (" " + additionalClasses) : "")) ;
 }
 
-function flexIEC(divValue, reqOptions)
+function testHeight(a, divValue) {
+
+    a.style.visibility = "hidden";
+    a.style.overflow = "auto";
+    divValue.appendChild(a);
+
+    var children = a.children;
+    for(var i=0; i < children.length; i++)
+    {
+    	console.log('', i, ' height: ', children[i].offsetHeight);
+    }
+
+    var height = a.offsetHeight;
+    console.log('h: ', height, ' or: ', a.clientHeight, ' or ', a.scrollHeight);
+    //get rid of this hidden object -- we don't need it anymore
+    divValue.removeChild(a);
+
+    return height;
+}
+var evoWrapHeight = 47;
+function createEvoWrap(eID)
+{
+	var bottomRow = createDiv(eID + "-bot", "row mOR border");
+	bottomRow.style.height = evoWrapHeight + "px";
+
+	return bottomRow;
+}
+
+function createGenericButtons(eID)
+{
+	return {"like" : createDiv(eID + "-like", "col-auto pOR border", "div", "like"), "publish" : createDiv(eID + "-publish", "col-auto pOR border", "div", "pub")};
+}
+
+
+
+
+function flexIEC()//divValue, reqOptions)
 {
 	// console.log(divValue);
 
 	var self = this;
 
+	self.eventManager = {};
+
+	Emitter(self);
+
 	//lets subdivide our div object according to function
 
-	var container = createDiv("test", "container fullWH containerOR");
-	
-	var row = createRow("test", "fullWH");
-
-	//this holds all parent info
-	var parentColumn = createDiv("parent-col", "col-xs-3 fullWH colObject border");
-
-	var parentTopRow = createDiv("p-top", "");
-
-	var choice = createDiv("p-top-choice", "border");
-	choice.innerHTML = "Parent Artifacts";
-
-	//add the choice object to our top row (to have something to display)
-	parentTopRow.appendChild(choice);
-
-	var parentBottomRow = createDiv("p-bot", "innerObject colObject");
-	var innerParent = createDiv("p-inner", "innerObject special");
-	// innerParent.style.flex = 1;
-	parentBottomRow.appendChild(innerParent);
-
-	//append both top and bottom to our children
-	parentColumn.appendChild(parentTopRow);
-	parentColumn.appendChild(parentBottomRow);
-	//add parent to the second row
-	// leftParentRow.appendChild(parentColumn);
-	
-
-	var rightColumn = createDiv("evo-col", "col-auto fullWH colObject border")
-
-	// var evoTopColumn = createDiv("p-top", "");
-
-	// var choice = createDiv("p-top-choice", "border");
-	// choice.innerHTML = "Parent Artifacts";
-	var tabs = createDiv("evoTabs", "tabs row");
-	
-	var aRef = createDiv("", "active col-auto border", "a", "Tab 1");
-	aRef.href = "#";
-	tabs.appendChild(aRef);
-	
-	aRef = createDiv("", "col-auto border", "a", "Tab 2");
-	aRef.href = "#";
-	tabs.appendChild(aRef);
-
-	aRef = createDiv("", "col-auto border", "a", "Tab 3");
-	aRef.href = "#";
-	tabs.appendChild(aRef);
-
-	var evoBottom = createDiv("evo-bot", "innerObject colObject");
-	var innerEvo = createDiv("evo-inner", "innerObject special");
-	evoBottom.appendChild(innerEvo);
-
-	rightColumn.appendChild(tabs);
-	rightColumn.appendChild(evoBottom);
-
-	//<div class="tabs"> <a href="#" class="active">Tab 1</a> <a href="#" style="margin:0 17px">Tab 2</a> <a href="#">Tab 3</a> </div>
-
-	//append both children
-	row.appendChild(parentColumn);
-	row.appendChild(rightColumn);
-
-	container.appendChild(row);
-
-	//then pull in the full containers
-	divValue.appendChild(container);
-
-	self.addFlexToPage = function(divToAppend, addOptions)
+	self.injectIEC = function(divValue, reqOptions)
 	{
-		var flexOptions = 
+		reqOptions = reqOptions || {};
+		//deep clone the options before sending it in
+		reqOptions = JSON.parse(JSON.stringify(reqOptions));
+
+		var container = createDiv("test", "container fullWH pOR");	
+		var row = createRow("test", "fullWH");
+
+		//this holds all parent info
+		var parentColumn = createDiv("parent-col", "col-xs-3 fullWH colObject border mOR pOR");
+
+		var parentTopRow = createDiv("p-top", "");
+
+		var choice = createDiv("p-top-choice", "border");
+		choice.innerHTML = "Parent Artifacts";
+
+		//add the choice object to our top row (to have something to display)
+		parentTopRow.appendChild(choice);
+
+		var parentBottomRow = createDiv("p-bot", "innerObject colObject");
+		var innerParent = createDiv("p-inner", "innerObject special");
+		// innerParent.style.flex = 1;
+		parentBottomRow.appendChild(innerParent);
+
+		//append both top and bottom to our children
+		parentColumn.appendChild(parentTopRow);
+		parentColumn.appendChild(parentBottomRow);
+		//add parent to the second row
+		// leftParentRow.appendChild(parentColumn);
+		
+	 
+		var rightColumn = createDiv("evo-col", "col-auto fullWH colObject border mOR pOR")
+
+		// var evoTopColumn = createDiv("p-top", "");
+
+		// var choice = createDiv("p-top-choice", "border");
+		// choice.innerHTML = "Parent Artifacts";
+		var tabs = createDiv("evoTabs", "tabs row mOR");
+		
+		var aRef = createDiv("", "active col-auto border", "a", "Tab 1");
+		aRef.href = "#";
+		tabs.appendChild(aRef);
+		
+		aRef = createDiv("", "col-auto border", "a", "Tab 2");
+		aRef.href = "#";
+		tabs.appendChild(aRef);
+
+		aRef = createDiv("", "col-auto border", "a", "Tab 3");
+		aRef.href = "#";
+		tabs.appendChild(aRef);   
+
+		var evoBottom = createDiv("evo-bot", "innerObject colObject");
+		var innerEvo = createDiv("evo-inner", "innerObject special");
+		evoBottom.appendChild(innerEvo);
+
+		rightColumn.appendChild(tabs);
+		rightColumn.appendChild(evoBottom);
+
+		//<div class="tabs"> <a href="#" class="active">Tab 1</a> <a href="#" style="margin:0 17px">Tab 2</a> <a href="#">Tab 3</a> </div>
+
+		//append both children
+		row.appendChild(parentColumn);
+		row.appendChild(rightColumn);
+
+		container.appendChild(row);
+
+		//then pull in the full containers
+		divValue.appendChild(container);
+
+		//add in our inner slide objects
+		self.addEvoFlexToPage("parent", innerParent, {objectSize: reqOptions.objectSize, mode: 'vertical', extraHeightPerObject: evoWrapHeight});
+		self.addEvoFlexToPage("iec", innerEvo, {objectSize: reqOptions.objectSize, extraHeightPerObject: evoWrapHeight});  
+	}
+
+	
+
+
+	self.likeElement = function(e)
+	{
+		var elementID = e.target.id.replace(/-like/g, "");
+		var el = document.getElementById(elementID);
+		console.log("Like: ", elementID, " el: ", el);
+
+		classes(el).toggle('like'); 
+
+		//we toggle adding this to our parent objects
+	}
+
+	self.publishElement = function(e)
+	{
+		console.log("Pub: ", e.target.id.replace(/-publish/g, ""));
+	}
+
+	self.finishElementCreated = function(eID, eDiv)
+	{
+		//stop the loading of this object
+		// console.log('Stop it! ', eID, ' div: ', eDiv);
+		
+		var c = classes(eDiv);
+		if(c.has('loading'))
+			c.toggle('loading');
+
+		for(var i=0; i < eDiv.children.length; i++)
+		{	
+			var c = classes(eDiv.children[i]);
+			if(c.has('loading'))
+				c.toggle('loading');
+		}
+	}
+
+	self.createGenericIndividual = function(eID)
+	{
+		var external = createDiv(eID + "-wrap", "colObject");
+
+		var loading = createLoadingDiv(eID + "-object", "innerObject");
+
+		//add the customary piece
+		var bottomRow = createEvoWrap(eID);
+		var bottomButtons = createGenericButtons(eID);
+		
+		bottomRow.appendChild(bottomButtons.like);
+		bottomRow.appendChild(bottomButtons.publish);
+
+		external.appendChild(loading);
+		external.appendChild(bottomRow);
+
+		return {individual: loading, full: external, buttons: bottomButtons};
+	} 
+
+	self.addEvoFlexToPage = function(flexName, divToAppend, addOptions)
+	{
+		var flexOptions =  
 		{
 			objectSize: {width: 250, height: 200, rowMargin: 4},
 			// mode: 'vertical' ,
 			width : 500
-
 		};
 
 		addOptions = addOptions || {};
@@ -158,28 +264,58 @@ function flexIEC(divValue, reqOptions)
 		for(var key in addOptions)
 			flexOptions[key] = addOptions[key];
 
-		var nf = flexforever(divToAppend, flexOptions)
+		var nf = new flexforever(divToAppend, flexOptions);
 
-		nf.on('elementVisible', function(eID, eDiv)
+	
+		nf.on('elementCreated', function(eID, eDiv)
 		{
 			// console.log('EVisible id: ', eID, " div: ", eDiv);
+			// element.className += "grid-cell";	
+			var elementObj = createGenericIndividual(eID);
+
+			var likeManager = events(elementObj.buttons.like, self);
+			var pubManager = events(elementObj.buttons.publish, self);
+
+			self.eventManager[eID] = {like: likeManager, publish: pubManager};
+
+			likeManager.bind('click', 'likeElement');
+			pubManager.bind('click', 'publishElement');
+
+			eDiv.appendChild(elementObj.full);
+
+			self.emit('createIndividual', flexName, eID, elementObj.individual, self.finishElementCreated);
+
+		});
+
+		//when things are shown, pass it along
+		nf.on('elementVisible', function(eID, eDiv)
+		{
+			// console.log('evis emit- ', eID, eDiv);
+			self.emit('visibleIndividual', eID, document.getElementById(eID + "-object"));
+			// self.emit('elementVisible', eID, eDiv);
 			// element.className += "grid-cell";
-			eDiv.appendChild(createLoadingDiv(eID + "-loading", "fullWH"));
+			//removeChildren(eDiv);
 		});
 
 
+		//when things are hidden, pass it along
 		nf.on('elementHidden', function(eID, eDiv)
 		{
+			// console.log('ehid emit');
+			self.emit('hiddenIndividual', eID, document.getElementById(eID + "-object"));
+
+			// self.emit('elementHidden', eID, eDiv);
 			// element.className += "grid-cell";
-			removeChildren(eDiv);
+			//removeChildren(eDiv);
 		});
 
 		nf.createNewPage();
 	}
 
+	// var oAddHeight = testHeight(createEvoWrap("hidden-test"), divValue);
+	// console.log('Height to add: ', oAddHeight);
 	// self.addFlexToPage(parentColumn, {mode: 'vertical'});
-	self.addFlexToPage(innerParent, {mode: 'vertical'});
-	self.addFlexToPage(innerEvo);
+
 
 
 
@@ -195,7 +331,6 @@ function flexIEC(divValue, reqOptions)
 		// throw new Error("Can't use flexIEC without options or objectSize");
 
 	//add emitter properties to this object
-	Emitter(self);
 
 
 	//send us back please, we've got precious events to hook into
